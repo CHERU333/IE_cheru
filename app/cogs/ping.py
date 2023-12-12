23b62c2ad2f2c7f21fa457138c932eda83b75ddf
Fujii Rise <pi@cheru.jp>
2023-12-12 21:23:32 +0900

    偽ずんだもんとおみくじ

--- app/cogs/ping.py
+++ app/cogs/ping.py
@@ -1,14 +1,48 @@
+import discord
+import random
 from discord.ext import commands
 
-
-class Ping(commands.Cog):
+# ZundaCogクラスの定義
+class ZundaCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # pingコマンドの定義
     @commands.command()
-    async def ping(self,ctx):
-        await ctx.send("pong!")
+    async def ping(self, ctx):
+        await ctx.send("pong!")  # "pong!"と返す
 
+    # pingコマンドの定義
+    @commands.command()
+    async def greet(self, ctx, member: discord.Member = None):
+        if member is None:
+            await ctx.send(f"Hi {ctx.author.mention}!")  # メッセージにメンションを含めて送信
+        else:
+            await ctx.send(f"Hi {member.mention}!")
 
+    # おみくじコマンドの定義
+    @commands.command()
+    async def omikuji(self, ctx):
+        fortunes = [
+            "大吉",
+            "吉",
+            "中吉",
+            "小吉",
+            "末吉",
+            "凶",
+            "大凶",
+        ]
+        # おみくじの結果をランダムに選ぶ
+        result = random.choice(fortunes)
+        # 結果を送信
+        await ctx.send(f"おみくじの結果: **{result}**")
+
+    # zundaコマンドの定義
+    @commands.command()
+    async def zunda(self, ctx, *, message):
+        response = f"{message} なのだ"
+        await ctx.send(response)
+
+# setup関数の定義
 async def setup(bot):
-    await bot.add_cog(Ping(bot))
+    await bot.add_cog(ZundaCog(bot))  # ZundaCogクラスのインスタンスをボットに追加
--- app/main.py
+++ app/main.py
@@ -2,19 +2,21 @@ import discord
 from discord.ext import commands
 from mylib.constant import EXTENTIONS, TOKEN
 
-
+# MyBotクラスを定義
 class MyBot(commands.Bot):
     def __init__(self):
         super().__init__(
-            command_prefix="/",
-            intents=discord.Intents.all(),
+            command_prefix="/",  # コマンドのプレフィックスを指定
+            intents=discord.Intents.all(),  # すべてのインテントを使用
         )
 
+    # 拡張機能を一括で読み込むためのメソッド
     async def setup_hook(self):
-        for extention in EXTENTIONS:
-            await self.load_extension(extention)
+        for extension in EXTENTIONS:
+            await self.load_extension(extension)
 
-
+# メインの実行部分
 if __name__ == "__main__":
-    bot = MyBot()
-    bot.run(token=TOKEN)
+    bot = MyBot()  # MyBotクラスのインスタンスを作成
+    bot.run(token=TOKEN)  # ボットを実行するためのトークンを指定して実行
+
